c0162bc2d713a5ee52c1742f2d752d2fe499d1eb
Avneesh kumar <abhaykumary223@gmail.com>
2026-04-27 17:02:08 +0530

    Change API base URL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/ocr",
+  baseURL: "https://receiptlens.onrender.com",
 });
 
 export const uploadFile = (formData) =>
